0615dbe502f0a89b937c3d2abbd79b9e3fc2ba75
BioDani <dtejada@unal.edu.co>
2023-08-31 20:08:50 -0500

    corrección schema country
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- schemas/country_schema.py
+++ schemas/country_schema.py
@@ -5,15 +5,15 @@ from typing import Optional, List
 class Country(BaseModel):
     
     id: str = Field(min_length=1, max_length=15)
-    name_common: str = Field(min_length=3, max_length=200)
-    name_official: str = Field(min_length=3, max_length=200)
+    name_common: str = Field(min_length=3, max_length=1000)
+    name_official: str = Field(min_length=3, max_length=1000)
     independent: bool
-    languages: str = Field(min_length=3, max_length=15)
-    region: str = Field(min_length=3, max_length=15)
-    subregion: str = Field(min_length=3, max_length=15)
-    flag_svg: str = Field(min_length=3, max_length=15)
-    flag_icon: str = Field(min_length=3, max_length=15) 
-    population: float
+    languages: str = Field(min_length=3, max_length=1000)
+    region: str = Field(min_length=3, max_length=1000)
+    subregion: str = Field(min_length=3, max_length=1000)
+    flag_svg: str = Field(min_length=3, max_length=1000)
+    flag_icon: str = Field(min_length=3, max_length=1000) 
+    population: int
     gini: float
 
     class Config:
@@ -23,7 +23,7 @@ class Country(BaseModel):
                 'name_common': 'Colombia',
                 'name_official': 'Republic of Colombia',
                 'independent': True,
-                'languages': 'Colombia',
+                'languages': 'Spanish',
                 'region': 'Americas',
                 'subregion': 'South America',
                 'flag_svg': "https://flagcdn.com/co.svg",
